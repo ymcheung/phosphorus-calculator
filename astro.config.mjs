a8a97260f632bac693d7120e711aba0b524883f6
ymcheung <ymcheung.tw@me.com>
2023-03-19 19:23:47 +0800

    Add github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 import { defineConfig } from 'astro/config';
-
 import AstroPWA from '@vite-pwa/astro';
 import solidJs from "@astrojs/solid-js";
+
+import deno from "@astrojs/deno";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     logLevel: 'info',
     define: {
-      __DATE__: `'${new Date().toISOString()}'`,
-    },
+      __DATE__: `'${new Date().toISOString()}'`
+    }
   },
   integrations: [AstroPWA({
     mode: 'development',
@@ -21,32 +22,30 @@ export default defineConfig({
       name: 'Astro PWA',
       short_name: 'Astro PWA',
       theme_color: '#ffffff',
-      icons: [
-        {
-          src: 'pwa-192x192.png',
-          sizes: '192x192',
-          type: 'image/png',
-        },
-        {
-          src: 'pwa-512x512.png',
-          sizes: '512x512',
-          type: 'image/png',
-        },
-        {
-          src: 'pwa-512x512.png',
-          sizes: '512x512',
-          type: 'image/png',
-          purpose: 'any maskable',
-        },
-      ],
+      icons: [{
+        src: 'pwa-192x192.png',
+        sizes: '192x192',
+        type: 'image/png'
+      }, {
+        src: 'pwa-512x512.png',
+        sizes: '512x512',
+        type: 'image/png'
+      }, {
+        src: 'pwa-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any maskable'
+      }]
     },
     workbox: {
       navigateFallback: '/404',
-      globPatterns: ['**/*.{css,js,html,svg,png,ico,txt}'],
+      globPatterns: ['**/*.{css,js,html,svg,png,ico,txt}']
     },
     devOptions: {
       enabled: true,
-      navigateFallbackAllowlist: [/^\/404$/],
-    },
-  }), solidJs()]
+      navigateFallbackAllowlist: [/^\/404$/]
+    }
+  }), solidJs()],
+  output: "server",
+  adapter: deno()
 });

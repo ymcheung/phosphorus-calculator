d7c70f1f4cf8a2f27df3daca3e4e2a3078ed3c19
ymcheung <ymcheung.tw@me.com>
2023-03-19 11:24:21 +0800

    Add vite-pwa

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,52 @@
 import { defineConfig } from 'astro/config';
 
+import AstroPWA from '@vite-pwa/astro';
+import solidJs from "@astrojs/solid-js";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    logLevel: 'info',
+    define: {
+      __DATE__: `'${new Date().toISOString()}'`,
+    },
+  },
+  integrations: [AstroPWA({
+    mode: 'development',
+    base: '/',
+    scope: '/',
+    includeAssets: ['favicon.svg'],
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'Astro PWA',
+      short_name: 'Astro PWA',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any maskable',
+        },
+      ],
+    },
+    workbox: {
+      navigateFallback: '/404',
+      globPatterns: ['**/*.{css,js,html,svg,png,ico,txt}'],
+    },
+    devOptions: {
+      enabled: true,
+      navigateFallbackAllowlist: [/^\/404$/],
+    },
+  }), solidJs()]
+});
